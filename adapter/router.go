2cf7d184f0cae6acac307d64b1859539575de429
PuerNya <safarier@outlook.com>
2024-03-18 18:18:51 +0800

    Add `dns.rule[i].fallback_rules`

--- adapter/router.go
+++ adapter/router.go
@@ -97,6 +97,7 @@ type Rule interface {
 
 type DNSRule interface {
 	Rule
+	MatchFallback(metadata *InboundContext, index int) (bool, string, string, int)
 	DisableCache() bool
 	RewriteTTL() *uint32
 	ClientSubnet() *netip.Prefix
