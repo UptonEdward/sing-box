KHTdhl/sing-box-k

Add multi-server support for dns rule

option/rule_dns.go
@@ -16,7 +16,7 @@ type FallBackRule struct {
 	RuleSet     Listable[string] `json:"rule_set,omitempty"`
 	IPIsPrivate bool             `json:"ip_is_private,omitempty"`
 	Invert      bool             `json:"invert,omitempty"`
-	Server      string           `json:"server,omitempty"`
+	Server      Listable[string] `json:"server,omitempty"`
 }
 
 func (r FallBackRule) IsValid() bool {
@@ -119,7 +119,7 @@ type DefaultDNSRule struct {
 	RuleSet                  Listable[string]       `json:"rule_set,omitempty"`
 	RuleSetIPCIDRMatchSource bool                   `json:"rule_set_ipcidr_match_source,omitempty"`
 	Invert                   bool                   `json:"invert,omitempty"`
-	Server                   string                 `json:"server,omitempty"`
+	Server                   Listable[string]       `json:"server,omitempty"`
 	DisableCache             bool                   `json:"disable_cache,omitempty"`
 	RewriteTTL               *uint32                `json:"rewrite_ttl,omitempty"`
 	ClientSubnet             *ListenAddress         `json:"client_subnet,omitempty"`
@@ -136,13 +136,13 @@ func (r DefaultDNSRule) IsValid() bool {
 }
 
 type LogicalDNSRule struct {
-	Mode         string         `json:"mode"`
-	Rules        []DNSRule      `json:"rules,omitempty"`
-	Invert       bool           `json:"invert,omitempty"`
-	Server       string         `json:"server,omitempty"`
-	DisableCache bool           `json:"disable_cache,omitempty"`
-	RewriteTTL   *uint32        `json:"rewrite_ttl,omitempty"`
-	ClientSubnet *ListenAddress `json:"client_subnet,omitempty"`
+	Mode         string           `json:"mode"`
+	Rules        []DNSRule        `json:"rules,omitempty"`
+	Invert       bool             `json:"invert,omitempty"`
+	Server       Listable[string] `json:"server,omitempty"`
+	DisableCache bool             `json:"disable_cache,omitempty"`
+	RewriteTTL   *uint32          `json:"rewrite_ttl,omitempty"`
+	ClientSubnet *ListenAddress   `json:"client_subnet,omitempty"`
 }
 
 func (r LogicalDNSRule) IsValid() bool {

UptonEdward/sing-box

option/rule_dns.go
@@ -16,7 +16,7 @@ type FallBackRule struct {
 	RuleSet     Listable[string] `json:"rule_set,omitempty"`
 	IPIsPrivate bool             `json:"ip_is_private,omitempty"`
 	Invert      bool             `json:"invert,omitempty"`
-	Server      string           `json:"server,omitempty"`
+	Server      Listable[string] `json:"server,omitempty"`
 }
 
 func (r FallBackRule) IsValid() bool {
@@ -119,7 +119,7 @@ type DefaultDNSRule struct {
 	RuleSet                  Listable[string]       `json:"rule_set,omitempty"`
 	RuleSetIPCIDRMatchSource bool                   `json:"rule_set_ipcidr_match_source,omitempty"`
 	Invert                   bool                   `json:"invert,omitempty"`
-	Server                   string                 `json:"server,omitempty"`
+	Server                   Listable[string]       `json:"server,omitempty"`
 	DisableCache             bool                   `json:"disable_cache,omitempty"`
 	RewriteTTL               *uint32                `json:"rewrite_ttl,omitempty"`
 	ClientSubnet             *AddrPrefix            `json:"client_subnet,omitempty"`
@@ -136,13 +136,13 @@ func (r DefaultDNSRule) IsValid() bool {
 }
 
 type LogicalDNSRule struct {
-	Mode         string      `json:"mode"`
-	Rules        []DNSRule   `json:"rules,omitempty"`
-	Invert       bool        `json:"invert,omitempty"`
-	Server       string      `json:"server,omitempty"`
-	DisableCache bool        `json:"disable_cache,omitempty"`
-	RewriteTTL   *uint32     `json:"rewrite_ttl,omitempty"`
-	ClientSubnet *AddrPrefix `json:"client_subnet,omitempty"`
+	Mode         string           `json:"mode"`
+	Rules        []DNSRule        `json:"rules,omitempty"`
+	Invert       bool             `json:"invert,omitempty"`
+	Server       Listable[string] `json:"server,omitempty"`
+	DisableCache bool             `json:"disable_cache,omitempty"`
+	RewriteTTL   *uint32          `json:"rewrite_ttl,omitempty"`
+	ClientSubnet *AddrPrefix      `json:"client_subnet,omitempty"`
 }
 
 func (r LogicalDNSRule) IsValid() bool {